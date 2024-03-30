8e640a1dfd99acab7925c836c0fce2511f7dd53a
Danillo Guimarães <151641788+Danilloneo@users.noreply.github.com>
2024-03-30 00:25:15 -0300

    Criado e Ajustado * style, ajustado cabecalho, ajustado hero styled components

---
+++ src/components/Cabecalho/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import { colors } from '../../styles'
+
+const CabecalhoEstilo = styled.header`
+  background-color: ${colors.corSecundaria};
+  color: ${colors.corPrincipal};
+  text-align: center;
+  padding: 24px 0;
+`
+export default CabecalhoEstilo
